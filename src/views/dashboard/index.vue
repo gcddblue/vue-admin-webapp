f588626cf41bf7537d1ea228ca28b173fbc5b5c1
gch <1024484268@qq.com>
2019-07-04 23:40:06 +0800

    add vue-count-to

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -2,27 +2,84 @@
   <div class="dashbord">
     <el-row class="infoCrads">
       <el-col :span="6">
-        <div class="cardItem"></div>
+        <div class="cardItem">
+          <div class="cardItem_txt">
+            <count-to
+              class="cardItem_p0"
+              :startVal="startVal"
+              :endVal="endVal1"
+              :duration="2000"
+            ></count-to>
+            <p class="cardItem_p1">Total Visitors</p>
+          </div>
+          <div class="cardItem_icon">
+            <i class="el-icon-user-solid"></i>
+          </div>
+        </div>
       </el-col>
       <el-col :span="6">
-        <div class="cardItem"></div>
+        <div class="cardItem">
+          <div class="cardItem_txt">
+            <count-to
+              class="cardItem_p0"
+              :startVal="startVal"
+              :endVal="endVal1"
+              :duration="2000"
+            ></count-to>
+            <p class="cardItem_p1">Total Order Placeed</p>
+          </div>
+          <div class="cardItem_icon">
+            <i class="el-icon-user-solid"></i>
+          </div>
+        </div>
       </el-col>
       <el-col :span="6">
-        <div class="cardItem"></div>
+        <div class="cardItem">
+          <div class="cardItem_txt">
+            <count-to
+              class="cardItem_p0"
+              :startVal="startVal"
+              :endVal="endVal1"
+              :duration="2000"
+            ></count-to>
+            <p class="cardItem_p1">Total Visitors</p>
+          </div>
+          <div class="cardItem_icon">
+            <i class="el-icon-user-solid"></i>
+          </div>
+        </div>
       </el-col>
       <el-col :span="6">
-        <div class="cardItem"></div>
+        <div class="cardItem">
+          <div class="cardItem_txt">
+            <count-to
+              class="cardItem_p0"
+              :startVal="startVal"
+              :endVal="endVal1"
+              :duration="2000"
+            ></count-to>
+            <p class="cardItem_p1">Total Visitors</p>
+          </div>
+          <div class="cardItem_icon">
+            <i class="el-icon-user-solid"></i>
+          </div>
+        </div>
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
+import CountTo from 'vue-count-to'
 export default {
   data() {
     return {
-      num: ''
+      startVal: 0,
+      endVal1: 27800
     }
+  },
+  components: {
+    CountTo
   }
 }
 </script>
@@ -36,6 +93,28 @@ export default {
     .cardItem {
       height: 108px;
       background: #fff;
+    }
+  }
+}
+.cardItem {
+  color: #666;
+  .cardItem_txt {
+    float: left;
+    margin: 26px 0 0 15px;
+    .cardItem_p0 {
+      font-size: 20px;
+      color: #009688;
+    }
+    .cardItem_p1 {
+      font-size: 16px;
+    }
+  }
+  .cardItem_icon {
+    float: right;
+    margin: 24px 14px 0 0;
+    i {
+      font-size: 55px;
+      color: #009688;
     }
   }
 }
