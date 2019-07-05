a4583e757a35e7f8c87905cb8cebbcc940ba8092
高晨辉 <gaochenhui@xinnet.com>
2019-07-05 17:44:08 +0800

    dashboard add echarts

---
+++ src/views/dashboard/components/LineCharts.vue
@@ -0,0 +1,88 @@
+<template>
+  <div
+    class="lineCharts"
+    :style="{ width: width, height: height }"
+    ref="myCharts"
+  ></div>
+</template>
+
+<script>
+import echarts from 'echarts'
+export default {
+  props: {
+    width: {
+      type: String,
+      default: '100%'
+    },
+    height: {
+      type: String,
+      default: '350px'
+    }
+  },
+  data() {
+    return {
+      mycharts: null
+    }
+  },
+  mounted() {
+    this.$nextTick().then(() => {
+      this.initEcharts()
+    })
+  },
+  methods: {
+    initEcharts() {
+      this.mycharts = echarts.init(this.$refs.myCharts)
+      this._setOtion()
+    },
+    _setOtion() {
+      this.mycharts.setOption({
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              background: '#6a7985'
+            }
+          }
+        },
+        legend: {
+          data: ['收入', '支出']
+        },
+        grid: {
+          left: '20',
+          right: '20',
+          bottom: '3',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            type: 'category',
+            boundaryGap: false,
+            data: ['1月', '2月', '3月', '4月', '5月', '6月']
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ],
+        series: [
+          {
+            name: '收入',
+            type: 'line',
+            areaStyle: {},
+            data: [9000, 12000, 30000, 24000, 12000, 40000]
+          },
+          {
+            name: '支出',
+            type: 'line',
+            areaStyle: {},
+            data: [4000, 6000, 30000, 8000, 2000, 10000]
+          }
+        ]
+      })
+    }
+  }
+}
+</script>
+<style></style>
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="dashbord">
+    <!-- cardlist -->
     <el-row class="infoCrads">
       <el-col :span="6">
         <div class="cardItem">
           <div class="cardItem_txt">
             <count-to
-              class="cardItem_p0"
+              class="cardItem_p0 color-green1"
               :startVal="startVal"
               :endVal="endVal1"
               :duration="2000"
@@ -13,7 +14,7 @@
             <p class="cardItem_p1">Total Visitors</p>
           </div>
           <div class="cardItem_icon">
-            <i class="el-icon-user-solid"></i>
+            <i class="el-icon-user color-green1"></i>
           </div>
         </div>
       </el-col>
@@ -21,15 +22,31 @@
         <div class="cardItem">
           <div class="cardItem_txt">
             <count-to
-              class="cardItem_p0"
+              class="cardItem_p0 color-blue"
               :startVal="startVal"
-              :endVal="endVal1"
+              :endVal="endVal2"
+              :duration="2000"
+            ></count-to>
+            <p class="cardItem_p1">Messages</p>
+          </div>
+          <div class="cardItem_icon">
+            <i class="el-icon-s-comment color-blue"></i>
+          </div>
+        </div>
+      </el-col>
+      <el-col :span="6">
+        <div class="cardItem">
+          <div class="cardItem_txt">
+            <count-to
+              class="cardItem_p0 color-red"
+              :startVal="startVal"
+              :endVal="endVal3"
               :duration="2000"
             ></count-to>
             <p class="cardItem_p1">Total Order Placeed</p>
           </div>
           <div class="cardItem_icon">
-            <i class="el-icon-user-solid"></i>
+            <i class="el-icon-shopping-cart-2 color-red"></i>
           </div>
         </div>
       </el-col>
@@ -37,57 +54,64 @@
         <div class="cardItem">
           <div class="cardItem_txt">
             <count-to
-              class="cardItem_p0"
+              class="cardItem_p0 color-green2"
               :startVal="startVal"
-              :endVal="endVal1"
+              :endVal="endVal4"
               :duration="2000"
             ></count-to>
-            <p class="cardItem_p1">Total Visitors</p>
+            <p class="cardItem_p1">Total Profit</p>
           </div>
           <div class="cardItem_icon">
-            <i class="el-icon-user-solid"></i>
-          </div>
-        </div>
-      </el-col>
-      <el-col :span="6">
-        <div class="cardItem">
-          <div class="cardItem_txt">
-            <count-to
-              class="cardItem_p0"
-              :startVal="startVal"
-              :endVal="endVal1"
-              :duration="2000"
-            ></count-to>
-            <p class="cardItem_p1">Total Visitors</p>
-          </div>
-          <div class="cardItem_icon">
-            <i class="el-icon-user-solid"></i>
+            <i class="el-icon-wallet color-green2"></i>
           </div>
         </div>
       </el-col>
     </el-row>
+    <!-- end -->
+    <!-- lineEcharts -->
+    <div class="whiteBox"></div>
+    <line-charts class="lCharts"></line-charts>
+    <!-- end -->
   </div>
 </template>
 
 <script>
 import CountTo from 'vue-count-to'
+import LineCharts from './components/LineCharts'
 export default {
   data() {
     return {
       startVal: 0,
-      endVal1: 27800
+      endVal1: 27800,
+      endVal2: 200,
+      endVal3: 1000,
+      endVal4: 102200
     }
   },
   components: {
-    CountTo
+    CountTo,
+    LineCharts
   }
 }
 </script>
 <style scoped lang="scss">
+.color-green1 {
+  color: #40c9c6 !important;
+}
+.color-blue {
+  color: #36a3f7 !important;
+}
+.color-red {
+  color: #f4516c !important;
+}
+.color-green2 {
+  color: #34bfa3 !important;
+}
 .dashbord {
-  padding: 36px 10px;
+  padding: 36px 30px;
 }
 .infoCrads {
+  margin: 0 -20px 0 -20px;
   .el-col {
     padding: 0 20px;
     .cardItem {
@@ -98,12 +122,13 @@ export default {
 }
 .cardItem {
   color: #666;
+  box-shadow: 0 0 10px #e2e2e2;
   .cardItem_txt {
     float: left;
-    margin: 26px 0 0 15px;
+    margin: 26px 0 0 20px;
     .cardItem_p0 {
       font-size: 20px;
-      color: #009688;
+      font-weight: bold;
     }
     .cardItem_p1 {
       font-size: 16px;
@@ -111,11 +136,14 @@ export default {
   }
   .cardItem_icon {
     float: right;
-    margin: 24px 14px 0 0;
+    margin: 24px 20px 0 0;
     i {
       font-size: 55px;
-      color: #009688;
     }
   }
+}
+.lCharts {
+  background: #fff;
+  margin-top: 30px;
 }
 </style>
