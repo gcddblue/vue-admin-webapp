9dbe8889cbf565fc0050e750fa0cc3e8822d28fd
高晨辉 <gaochenhui@xinnet.com>
2019-07-17 18:00:38 +0800

    table add

--- src/views/table/complex-table.vue
+++ src/views/table/complex-table.vue
@@ -4,6 +4,43 @@
       <div slot="header">
         <span>复杂操作表格</span>
       </div>
+      <div class="searchDiv">
+        <el-input
+          type="text"
+          placeholder="请输入订单号"
+          class="width1"
+          v-model="sch_order"
+        ></el-input>
+        <el-select
+          v-model="sch_status"
+          clearable
+          class="width1"
+          placeholde="请选择状态"
+        >
+          <el-option
+            v-for="item in options"
+            :label="item.label"
+            :value="item.value"
+            :key="item.value"
+          ></el-option>
+        </el-select>
+        <el-date-picker
+          class="width1"
+          v-model="sch_date"
+          type="date"
+          placeholder="选择日期时间"
+          value-format="yyyy-MM-dd"
+        ></el-date-picker>
+        <el-button type="primary" icon="el-icon-search" @click="searchTab()"
+          >搜索</el-button
+        >
+        <el-button
+          type="primary"
+          icon="el-icon-circle-plus-outline"
+          @click="addTab"
+          >添加</el-button
+        >
+      </div>
       <el-table :data="tableData" border stripe>
         <el-table-column prop="id" label="序号" width="80"></el-table-column>
         <el-table-column prop="order" label="订单号"></el-table-column>
@@ -117,7 +154,7 @@
           </el-select>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="changeTab('diaForm')"
+          <el-button type="primary" @click="changeTab('diaForm', editType)"
             >确认</el-button
           >
           <el-button @click="diaIsShow = false">取消</el-button>
@@ -134,12 +171,17 @@ export default {
     return {
       tableData: [],
       allList: [],
+      schArr: [],
+      sch_order: '',
+      sch_status: null,
+      sch_date: null,
       currentPage: 1,
       pageSize: 10,
       total: 0,
       pageSizes: [10, 20, 30, 40],
       diaIsShow: false,
       formData: {},
+      editType: '',
       options: [
         { label: '待审核', value: 1 },
         { label: '配送中', value: 2 },
@@ -208,6 +250,7 @@ export default {
       getPageTab2()
         .then(res => {
           this.allList = res.data.tableList
+          this.schArr = this.allList
           this.getPageData()
           this.total = res.data.total
         })
@@ -218,8 +261,45 @@ export default {
     getPageData() {
       let start = (this.currentPage - 1) * this.pageSize
       let end = start + this.pageSize
-      this.tableData = this.allList.slice(start, end)
+      this.tableData = this.schArr.slice(start, end)
     },
+    // 查找
+    searchTab() {
+      let arrList = []
+      for (let item of this.allList) {
+        if (
+          this.sch_order.trim() === '' &&
+          this.sch_status === null &&
+          this.sch_date === null
+        ) {
+          arrList = this.allList
+          break
+        } else if (
+          item.order.startsWith(this.sch_order) &&
+          (this.sch_status !== null ? item.status === this.sch_status : true) &&
+          (this.sch_date !== null ? item.time.startsWith(this.sch_date) : true)
+        ) {
+          let obj = Object.assign({}, item)
+          arrList.push(obj)
+        }
+      }
+      this.schArr = arrList
+      this.total = arrList.length
+      this.currentPage = 1
+      this.pageSize = 10
+      this.getPageData()
+    },
+    // add
+    addTab() {
+      this.formData = {}
+      this.formData
+      this.diaIsShow = true
+      this.editType = 'add'
+      this.$nextTick(() => {
+        this.$refs.diaForm.clearValidate()
+      })
+    },
+    // 审核
     toConfirm(row) {
       row.status = 2
       this.$notify({
@@ -228,6 +308,7 @@ export default {
         type: 'success'
       })
     },
+    // 完成
     toSuccess(row) {
       row.status = 0
       this.$notify({
@@ -236,6 +317,7 @@ export default {
         type: 'success'
       })
     },
+    // 取消
     toDelete(row) {
       row.status = 3
       this.$notify({
@@ -244,26 +326,40 @@ export default {
         type: 'success'
       })
     },
+    // 编辑
     editTable(index, row) {
       this.formData = Object.assign({}, row)
+      this.editType = 'update'
       this.diaIsShow = true
+      this.$nextTick(() => {
+        this.$refs.diaForm.clearValidate()
+      })
       this.rowIndex = index
     },
-    changeTab(form) {
+    changeTab(form, type) {
       this.$refs[form].validate(valid => {
         if (valid) {
-          // this.tableData[this.rowIndex] = Object.assign({}, this.formData)
-          // let newArr = Object.assign({}, this.tableData)
-          this.$set(
-            this.tableData,
-            this.rowIndex,
-            Object.assign({}, this.formData)
-          )
-          this.$notify({
-            title: '成功',
-            message: '订单已修改成功',
-            type: 'success'
-          })
+          if (type === 'update') {
+            // 改变整个表格数据
+            let start = (this.currentPage - 1) * this.pageSize
+            this.allList[start + this.rowIndex] = Object.assign(
+              {},
+              this.formData
+            )
+            // 解决数组不能通过索引响应数据变化
+            this.$set(
+              this.tableData,
+              this.rowIndex,
+              Object.assign({}, this.formData)
+            )
+            this.$notify({
+              title: '成功',
+              message: '订单已修改成功',
+              type: 'success'
+            })
+          } else {
+            alert('hhh')
+          }
           this.diaIsShow = false
         } else {
           return
@@ -279,9 +375,19 @@ export default {
   margin-top: 30px;
   padding-bottom: 20px;
 }
-.anoCard .el-button {
+.anoCard .el-table .el-button {
   padding: 8px 18px;
   font-size: 12px;
+}
+.searchDiv {
+  margin-bottom: 20px;
+  .el-button {
+    padding: 11px 20px;
+  }
+}
+.width1 {
+  width: 180px;
+  margin-right: 10px;
 }
 .diaForm {
   .el-input {
@@ -302,5 +408,8 @@ export default {
 }
 .diaForm .el-form-item__label {
   padding-right: 20px;
+}
+.searchDiv [class^='el-icon'] {
+  color: #fff;
 }
 </style>
